697fbc77405a1d1bcb8201453c4b8fd31c3647ea
Cinthya <cinthyadasilvasoares@outlook.com>
2022-09-19 13:22:09 -0300

    reset css

--- forca.js
+++ forca.js
@@ -1,3 +1,17 @@
+//Seletores
+let palavras = ["GATO", "DOCE", "VIDA", "CARRO"];
+let tabuleiro = document.getElementById("forca").getContext("2d");
+let palavraSecreta = "";
+
+function sorteiaPalavras (){
+    let palavra = palavras[Math.floor(Math.random() * palavras.length)]
+    palavraSecreta = palavra
+    console.log(palavraSecreta)
+}
+
+
+
 function desapareceBotao() {
     document.getElementById("div-desaparece").style.display = "none";
+    sorteiaPalavras()
 }
