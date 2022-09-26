960221bce11c2e5bf7d976745934358e4ec1bf79
Cinthya <cinthyadasilvasoares@outlook.com>
2022-09-26 11:16:29 -0300

    fazendo o reconhecimento das letras corretas e incorretas desehando na tela

--- canva.js
+++ canva.js
@@ -22,9 +22,27 @@ function desenharLinhas() {
     tabuleiro.beginPath();
     let largura = 600/palavraSecreta.length
     for(let i = 0; i < palavraSecreta.length; i++){
-        tabuleiro.moveTo(500 + (largura*i), 640);
-        tabuleiro.lineTo(550 + (largura*i), 640);
+        tabuleiro.moveTo(500 + (largura*i), 640)
+        tabuleiro.lineTo(550 + (largura*i), 640)
 }
-    tabuleiro.stroke();
-    tabuleiro.closePath();
+    tabuleiro.stroke()
+    tabuleiro.closePath()
+}
+
+function escreverLetraCorreta(index){
+    tabuleiro.font = "bold 52px Inter";
+    tabuleiro.lineCap = "round";
+    tabuleiro.fillStyle = "#0A3871";
+    tabuleiro.lineWidth = 6;
+    let largura = 600/palavraSecreta.length
+    tabuleiro.fillText(palavraSecreta[index], 505 + (largura * index), 620)
+
+}
+
+function escreverLetraIncorreta(letra, erros){
+    tabuleiro.font = "bold 40px Inter";
+    tabuleiro.lineCap = "round";
+    tabuleiro.fillStyle = "#";
+    tabuleiro.lineWidth = 6;
+    tabuleiro.fillText(letra, 520 +(40 * (10 - erros)), 720, 40);
 }
--- forca.js
+++ forca.js
@@ -3,6 +3,9 @@ let palavras = ["GATO", "DOCE", "VIDA", "CARRO"];
 let tabuleiro = document.getElementById("forca").getContext("2d");
 let palavraSecreta = "";
 
+let letras = [];
+let erros = 6;
+
 function sorteiaPalavras (){
     let palavra = palavras[Math.floor(Math.random() * palavras.length)]
     palavraSecreta = palavra
@@ -10,6 +13,29 @@ function sorteiaPalavras (){
     return palavra
 }
 
+//ver se é uma letra
+function verificarLetra (key){
+    let estado = false
+    if(key >=65 && letras.indexOf(key) || key <= 90 && letras.indexOf(key)){
+        letras.push(key)
+        console.log(key)
+        console.log(letras)
+        return estado
+    }
+    else{
+        estado = true
+        letras.push(key)
+        console.log(key)
+        console.log(letras, "if true")
+        return estado
+    }
+}
+
+//contar os erros
+function adicionarLetraIncorreta(){
+    erros -=1
+    console.log(erros)
+}
 
 function desapareceBotao() {
     document.getElementById("div-desaparece").style.display = "none";
@@ -17,4 +43,23 @@ function desapareceBotao() {
     
     desenharCanvas()
     desenharLinhas()
+
+    //capturar a tecla digitada
+    document.onkeydown = (e) => {
+        let letra = e.key.toUpperCase()
+
+        //ver se é uma letra
+        if(verificarLetra (letra) && palavraSecreta.includes(letra)){
+            for(let i = 0; i < palavraSecreta.length; i++){
+                if(palavraSecreta[i] === letra){
+                    escreverLetraCorreta(i)
+                }
+            }
+        } //escrever letra incorreta
+        else{
+            adicionarLetraIncorreta(letra)
+            escreverLetraIncorreta(letra, erros)
+        }
+
+    }
 }
